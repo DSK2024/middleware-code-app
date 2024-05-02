94f4726c1e6f330bc15680addc1eaabdd5391691
DSK <168667924+DSK2024@users.noreply.github.com>
2024-05-03 06:39:26 +0900

    몽고DB 추가 및 소스 변경
    몽고DB 연결자 추가.
    태그의 게더 변경.
    드라이버 하위 클래스 Send 파라메터 변경.
    등등.

--- app.js
+++ app.js
@@ -1,9 +1,10 @@
 //{app}\app.js
 import Controller from './lib/controller/Controller.js'
+import Mongodb from  './lib/Mongodb.js'
 
 console.log("middleware Start!");
 
-let scanner = new Controller({
+const scanner = new Controller({
     connType : Controller.CONN_TYPE.SERIAL,
     connector : {
         portname : "COM1",
@@ -12,27 +13,42 @@ let scanner = new Controller({
         stopbits:"1",
     },
     tags:[{
-        tagname : "레일1_투입부_바코드데이터"
+        tagname : "레일1_투입부_바코드데이터",
+        data_start_idx : 1,
+        data_length : 15,
     }]
 });
-let ioT = new Controller(({
-    connType : Controller.CONN_TYPE.UDP,
+const ioT = new Controller(({
+    connType : Controller.CONN_TYPE.TCP,
     connector : {
         ip:"127.0.0.1",
         port:"1234",
     },
     tags:[{
         tagname : "성형기1호 카운트",
-        address : 0x4e20,
+        data_start_idx : 0x4e20,
+        data_type : "DEC",
+        data_length: 2
     },{
         tagname : "성형기1호 온도",
-        address : 0x4e2a,
+        data_start_idx : 0x4e2a,
+        data_type : "DEC",
+        data_length: 2
     },{
         tagname : "성형기1호 습도",
-        address : 0x4e34,
+        data_start_idx : 0x4e34,
+        data_type : "DEC",
+        data_length: 2
     }]
 }))
 
-scanner.Open();
-ioT.Open();
+scanner.ReadStart();
+ioT.ReadStart();
 
+const mongoDB = setInterval(() => {
+    ioT.tags.forEach((tag)=>{
+        Mongodb.session.InsertOne(tag.tagName, `LastDttm:${tag.LastDttm}, data:${tag.Data}`);
+    })
+}, 1000);
+
+let mongo = new Mongodb();
